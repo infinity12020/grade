4c2531114509cc17a8d707ce35f411b5fd56af84
infinity12020 <dilshodov.abduvali@mail.ru>
2023-12-07 15:14:43 +0500

    grade

---
+++ script.js
@@ -0,0 +1,19 @@
+let grade = prompt ("оценки")
+
+switch (grade) {
+    case "A":
+        console.log("отлично");
+        break;
+
+    case "B" :
+    console.log("хорошо");    
+    break;
+case "C" :
+console.log("Удовлетворител ьно");
+break;
+case "D" :
+console.log("неудовлетворительно");
+break ;
+default : console.log("некорректная оценка");
+break ;
+}
